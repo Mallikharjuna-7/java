e8f6d4512913a60a7db496ef855a1103a63ac8db
Mallikharjuna-7 <mallikharjuna.xworkz@gmail.com>
2024-06-27 00:38:03 +0530

    Pre increment and Decrement 27-june-2024

---
+++ PreIncrementEnd.java
@@ -0,0 +1,10 @@
+public class PreIncrementEnd {
+
+        public static void main(String args[]){
+                int value = 200;
+
+                System.out.println("Number:"+value);
+                System.out.println("Post Increment:"+(++value));
+                System.out.println("Post Decrement:"+(--value));
+        }
+}
---
+++ PreIncrementStart.java
@@ -0,0 +1,10 @@
+public class PreIncrementStart {
+
+        public static void main(String args[]){
+                int value = 87;
+
+                System.out.println("Number:"+value);
+                System.out.println("Post Increment:"+(++value));
+                System.out.println("Post Decrement:"+(--value));
+        }
+}
