d903e5de303902ec324dfc64059895fcd85370ec
Mallikharjuna-7 <mallikharjuna.xworkz@gmail.com>
2024-06-27 00:43:53 +0530

    Increasing Number 27-june-2024

---
+++ Increase.java
@@ -0,0 +1,8 @@
+public class Increase {
+
+        public static void main(String args[]){
+                for (int index = 1; index <= 100; index++){
+                        System.out.println(index);
+                }
+        }
+}
