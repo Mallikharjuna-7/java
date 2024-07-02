99672719263eb785bf7dc097f18ec9454ce8bcfc
Mallikharjuna-7 <mallikharjuna.xworkz@gmail.com>
2024-07-02 10:39:23 +0530

    Invoking 5 methods 02-july-2024

---
+++ Windowss.java
@@ -0,0 +1,58 @@
+public class Windowss {
+
+        public static void room_window(int price){
+                
+                String window_type = "Room Window";
+                int no_of_windows = 2;
+
+                System.out.println("Window:"+window_type);
+                System.out.println("1 Windows Price:"+price);
+                System.out.println("No of Windows:"+no_of_windows);
+                int windows_price= price*no_of_windows;
+                System.out.println("Total Windows Price:"+windows_price);
+        }
+        public static void kitchen_window(int price){
+                
+                String window_type = "Kitchen Window";
+                int no_of_windows = 1;
+
+                System.out.println("Window:"+window_type);
+                System.out.println("1 Windows Price:"+price);
+                System.out.println("No of Windows:"+no_of_windows);
+                int windows_price= price*no_of_windows;
+                System.out.println("Total Windows Price:"+windows_price);
+        }
+        public static void hall_window(int price){
+                
+                String window_type = "Hall Windows";
+                int no_of_windows = 4;
+
+                System.out.println("Window:"+window_type);
+                System.out.println("1 Windows Price:"+price);
+                System.out.println("No of Windows:"+no_of_windows);
+                int windows_price= price*no_of_windows;
+                System.out.println("Total Windows Price:"+windows_price);
+        }
+        public static void second_room_window(int price){
+                
+                String window_type = "Second Room Window";
+                int no_of_windows = 2;
+
+                System.out.println("Window:"+window_type);
+                System.out.println("1 Windows Price:"+price);
+                System.out.println("No of Windows:"+no_of_windows);
+                int windows_price= price*no_of_windows;
+                System.out.println("Total Windows Price:"+windows_price);
+        }
+        public static void gods_room_window(int price){
+                
+                String window_type = "God Room Window";
+                int no_of_windows = 1;
+
+                System.out.println("Window:"+window_type);
+                System.out.println("1 Windows Price:"+price);
+                System.out.println("No of Windows:"+no_of_windows);
+                int windows_price= price*no_of_windows;
+                System.out.println("Total Windows Price:"+windows_price);
+        }
+}
---
+++ WindowssRunner.java
@@ -0,0 +1,12 @@
+public class WindowssRunner {
+
+        public static void main(String args[]){
+                Windowss.room_window(2000);
+                Windowss.kitchen_window(1500);
+                Windowss.hall_window(2000);
+                Windowss.second_room_window(2000);
+                Windowss.gods_room_window(2500);
+
+
+        }
+}
