1d1df75ef3fef1b8aeb82764ad69d4497a57bb59
Mallikharjuna-7 <mallikharjuna.xworkz@gmail.com>
2024-07-11 01:08:17 +0530

    Food items method accept 11-july-2024

---
+++ july11/Food.java
@@ -0,0 +1,143 @@
+public class Food {
+
+        public static void dosa(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 40;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void idly(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 10;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void puri(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 30;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void vada(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 10;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void omlet(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 20;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void samosa(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 12;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void mirchi_bajji(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 5;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void egg_puff(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 20;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void biryani(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 250;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void meals(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 75;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void chapathi(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 40;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void pizza(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 150;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void sandwich(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 80;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void burger(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 120;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void ice_cream(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 60;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void cake(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 300;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void chocolate(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 20;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void biscuit(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 10;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void gulab_jamun(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 20;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+        public static void jilebi(String name, int quantity){
+        System.out.println("Food item:"+name);
+        System.out.println("Quantity is:"+quantity);
+        int price = 10;
+        System.out.println(name+"Price is:"+price);
+        System.out.println("Total Price is:"+price*quantity);
+        }
+}
---
+++ july11/FoodRunner.java
@@ -0,0 +1,25 @@
+public class FoodRunner {
+
+        public static void main(String args[]){
+        Food.dosa("Dosa",2);
+        Food.idly("Idly",4);
+        Food.puri("Puri",3);
+        Food.vada("Vada",5);
+        Food.omlet("Omlet",2);
+        Food.samosa("Samosa",8);
+        Food.mirchi_bajji("Mirchi Bajji",5);
+        Food.egg_puff("Egg Puff",3);
+        Food.biryani("Biryani",4);
+        Food.meals("Meals",2);
+        Food.chapathi("Chapathi",4);
+        Food.pizza("Pizza",2);
+        Food.sandwich("Sandwich",3);
+        Food.burger("Burger",2);
+        Food.ice_cream("Icecream",4);
+        Food.cake("Cake",2);
+        Food.chocolate("Chocolate",12);
+        Food.biscuit("Biscuit",6);
+        Food.gulab_jamun("Gulab Jamun",6);
+        Food.jilebi("Jilebi",4);
+        }
+}
